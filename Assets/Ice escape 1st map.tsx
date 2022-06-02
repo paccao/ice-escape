09c99671658bc283c254a1202a2aff244a65ac3d
Joel Plumppu <16489683+paccao@users.noreply.github.com>
2022-06-02 22:01:30 +0200

    Initial commit, added map and assets

---
+++ Assets/Ice escape 1st map.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.5" name="Ice escape 1st map" tilewidth="32" tileheight="32" tilecount="180" columns="20">
+ <image source="IceTileset.png" width="640" height="288"/>
+</tileset>
